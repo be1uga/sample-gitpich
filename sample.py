3ab9d55f4c7eed9e0a1e0580ff8fed8c7bb0a651
Satto Sun <45897265+be1uga@users.noreply.github.com>
2019-03-15 13:18:14 +0900

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,11 @@
+class SampleHandler(BaseHTTPRequestHandler):
+   def do_GET(self):
+       self.send_response(200)
+       self.send_header('Content-type', 'text/plain; rset=utf-8')
+       self.end_headers()
+       self.wfile.write("Hello, HTTP!\n".encode())
+       
+if __name__ == '__main__':
+   server_address = ('', 8000)
+   httpd = HTTPServer(server_address, SampleHandler)
+   httpd.serve_forever()
